21515d852637eaf8f7be8b930dc2a46a25da92ee
Gustavo Alves <164105793+gualvesx@users.noreply.github.com>
2025-03-04 15:17:42 -0300

    Update mapa.tsx

--- pages/mapa.tsx
+++ pages/mapa.tsx
@@ -1,14 +1,15 @@
 // pages/mapa.tsx
 import dynamic from 'next/dynamic';
+import GraficoColetas from '../components/GraficoColetas';
 
 // Carregar o componente de mapa dinamicamente, sem SSR
 const MapaComponent = dynamic(() => import('../components/MapaComponent'), { ssr: false });
 
 const MapaPage = () => {
   return (
-    <div>
-      <h1>Página do Mapa</h1>
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
       <MapaComponent />
+      <GraficoColetas />
     </div>
   );
 };
